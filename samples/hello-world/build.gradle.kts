e18ab3b08c3dcb3feab8907c8c4ed4cac3d97648
Leon Linhart <themrmilchmann@gmail.com>
2022-06-15 17:37:02 +0200

    fix(samples): remove wrong import

--- samples/hello-world/build.gradle.kts
+++ samples/hello-world/build.gradle.kts
@@ -19,8 +19,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import io.github.themrmilchmann.gradle.publish.curseforge.*
-
 plugins {
     java
     id("io.github.themrmilchmann.ecj") version "0.1.0"
