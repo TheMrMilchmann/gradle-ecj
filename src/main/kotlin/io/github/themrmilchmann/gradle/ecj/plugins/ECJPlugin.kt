20133d338dd17a53c1de4c760f93be20751456d7
Leon Linhart <themrmilchmann@gmail.com>
2023-03-03 14:02:19 +0100

    chore: update code comments to better reflect the current implementation

--- src/main/kotlin/io/github/themrmilchmann/gradle/ecj/plugins/ECJPlugin.kt
+++ src/main/kotlin/io/github/themrmilchmann/gradle/ecj/plugins/ECJPlugin.kt
@@ -86,14 +86,6 @@ public class ECJPlugin : Plugin<Project> {
             options.isFork = true
             options.forkOptions.jvmArgumentProviders.add(ECJCommandLineArgumentProvider(ecjConfiguration))
 
-            /*
-             * forkOptions.executable is, unfortunately, not a property. Setting it eagerly here (at configuration time)
-             * would be a bad decision and could lead to ordering issues. Thus, we create a provider here instead.
-             * However, we still have to register this provider as task input for proper incremental builds.
-             * Unfortunately, it is not possible to replicate the functionality of @Nested for programmatically defined
-             * task inputs. Hence, we can only use the languageVersion for now. This could lead to some undesired cache
-             * hits but the chance should be low and there is very little risk of this being an issue.
-             */
             val defaultJavaCompiler = provider {
                 if (java.toolchain.languageVersion.orNull?.canCompileOrRun(REQUIRED_JAVA_VERSION) == true) {
                     javaToolchains.compilerFor(java.toolchain).orNull ?: error("Could not get launcher for toolchain: ${java.toolchain}")
@@ -105,10 +97,28 @@ public class ECJPlugin : Plugin<Project> {
             }
 
             if (GradleVersion.current() < GRADLE_8_0) {
-                /* Overwrite the javaCompiler to make sure that it is not inferred from the toolchain. */
+                /*
+                 * Overwrite the javaCompiler to make sure that it is not inferred from the toolchain.
+                 *
+                 * On Gradle 7.x, we cannot simply change the executable to point to the appropriate one if
+                 * `javaCompiler` is set. To work around this, we safe
+                 *
+                 * However, we still have to register this provider as task input for proper incremental builds.
+                 * Unfortunately, it is not possible to replicate the functionality of @Nested for programmatically
+                 * defined task inputs. Hence, we can only use the languageVersion for now. This could lead to some
+                 * undesired cache hits but the chance should be low and there is very little risk of this being an
+                 * issue.
+                 */
                 javaCompiler.set(provider { null })
                 inputs.property("javaLauncher", defaultJavaCompiler.map { it.metadata.languageVersion.asInt() })
             } else {
+                /*
+                 * On Gradle 8.0.2+, we can set the `javaCompiler` of the task and change the executable to point to
+                 * another toolchain tool. Because we are just using `java` instead of `javac` from the same toolchain,
+                 * we don't have to fiddle with incremental build support here.
+                 *
+                 * See https://github.com/gradle/gradle/issues/23990
+                 */
                 this.javaCompiler.convention(defaultJavaCompiler)
             }
 
@@ -116,6 +126,10 @@ public class ECJPlugin : Plugin<Project> {
             @Suppress("ObjectLiteralToLambda")
             doFirst(object : Action<Task> {
                 override fun execute(t: Task) {
+                    /*
+                     * On Gradle 7.x, `javaCompiler` will have no value. In this case, we resolve `defaultJavaCompiler`
+                     * and use its path. On Gradle 8.x, we can just `javaCompiler`.
+                     */
                     val javacExecutable = javaCompiler.orElse(defaultJavaCompiler).get().executablePath.asFile
 
                     /*
