e52237140b55f8bffe30fb43a643d4ae9f379a82
Leon Linhart <themrmilchmann@gmail.com>
2023-08-08 11:55:39 +0200

    build: update Foojay Toolchains Plugin {0.4.0 => 0.6.0}

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@
  */
 pluginManagement {
     plugins {
-        id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
     }
 
     includeBuild("build-logic")
