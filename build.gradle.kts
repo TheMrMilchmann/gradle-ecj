2f4669745251dc63df98b21551a5a62ec0ca0570
Leon Linhart <themrmilchmann@gmail.com>
2024-12-28 18:32:05 +0100

    build: configure `-Xjdk-release`

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,6 +52,8 @@ kotlin {
             compilerOptions.configure {
                 apiVersion = KotlinVersion.KOTLIN_1_8
                 languageVersion = KotlinVersion.KOTLIN_1_8
+
+                freeCompilerArgs.add("-Xjdk-release=8")
             }
         }
 
